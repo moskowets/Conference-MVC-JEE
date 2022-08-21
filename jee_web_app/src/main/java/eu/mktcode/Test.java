7f03168bf93f7e810e833a5f35ac34e3753255ce
Pavlo Moskovets <p.moskovets@gmail.com>
2022-08-21 16:27:49 +0200

    uploaded mysql model and dummy web app

---
+++ jee_web_app/src/main/java/eu/mktcode/Test.java
@@ -0,0 +1,22 @@
+package eu.mktcode;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hi")
+public class Test extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "Hi, Pavlo! This was deployed app" + "</h1>");
+        out.println("</body></html>");
+    }
+}
